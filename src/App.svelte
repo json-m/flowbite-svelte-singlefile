ab66b362189926bcadbb20f0954a14d715a81f99
jason <jason@xax.li>
2024-11-06 13:39:54 -0700

    init template

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>import "./app.css";
+import Counter from './lib/Counter.svelte'</script>
+
+<main>
+
+  <div class="card">
+    <Counter></Counter>
+  </div>
+
+</main>
+
+
+<style>
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+</style>
